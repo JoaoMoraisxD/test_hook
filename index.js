0087b7da8d8e8de143f5a0a08216259bc96f7d46
Joao Morais DEV <joaomoraisxd@gmail.com>
2025-02-13 15:22:02 -0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Carregar o certificado SSL e a chave privada
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/unicocontato.tech/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/unicocontato.tech/fullchain.pem')
+};
 
 app.use(express.json());
 
 app.post('/', (req, res) => {
-
-    const response = req.body['hub.challenge']
-    res.json({ value: response});
+    const response = req.body['hub.challenge'];
+    res.json({ value: response });
 });
 
-app.listen(port, () => {
-console.log(`API de destino rodando em httplocalhost:${port}`);
+https.createServer(options, app).listen(port, () => {
+    console.log(`API de destino rodando em https://localhost:${port}`);
 });
